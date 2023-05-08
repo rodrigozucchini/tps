66886d19d234a6b83c30afe25933678baa144a9d
unknown <rodrigozucchini1997@gmail.com>
2023-05-07 22:27:56 -0300

    cambios

---
+++ tp3/tp3ej1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numberIn;
+
+ptintf("Este es in ")
+
+int main(int argc, char const *argv[])
+{
+    int showNumber(numberIn) {
+        printf("")
+    }
+
+    return 0;
+}
